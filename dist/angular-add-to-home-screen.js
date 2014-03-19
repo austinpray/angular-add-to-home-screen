c53ea43ebdfd9bb30f2feb105803027a58e4bf2a
Austin Pray <austin@austinpray.com>
2014-03-19 17:37:48 -0500

    add example.css

--- dist/angular-add-to-home-screen.js
+++ dist/angular-add-to-home-screen.js
@@ -12,6 +12,47 @@ angular.module('angularAddToHomeScreen', [])
 
 'use strict';
 
+/**
+ * 
+ */
+angular.module('angularAddToHomeScreen')
+  .factory('$homeScreenDetector', [function(){
+    
+    var parser = new UAParser();
+
+    function getMajorVersion (version) {
+      return version.split('.')[0];
+    }
+
+    var Detector = function(options) {
+      angular.extend(this, options);
+      if(angular.isDefined(this.customUA)) {
+        parser.setUA(this.customUA);
+      }
+      this.result = parser.getResult();
+    };
+
+    Detector.prototype.safari = function () {
+      return this.result.browser.name === 'Mobile Safari';
+    };
+
+    Detector.prototype.iOS7 = function () {
+      return this.result.os.name === 'iOS' && getMajorVersion(this.result.os.version) === '7';
+    };
+
+    Detector.prototype.iOS6 = function () {
+      return this.result.os.name === 'iOS' && getMajorVersion(this.result.os.version) === '6';
+    };
+
+    Detector.prototype.device = function () {
+      return this.result.device.model;
+    };
+
+    return Detector;
+
+  }]);
+'use strict';
+
 angular.module('angularAddToHomeScreen')
   .directive('ngAddToHomeScreen', ['$homeScreenDetector', 'aathsLocales', function($homeScreenDetector, aathsLocales){
     var hydrateInstructions = function (hsdInstance) {
@@ -68,46 +109,4 @@ angular.module('angularAddToHomeScreen')
         }
       }
     };
-  }]);
-
-'use strict';
-
-/**
- * 
- */
-angular.module('angularAddToHomeScreen')
-  .factory('$homeScreenDetector', [function(){
-    
-    var parser = new UAParser();
-
-    function getMajorVersion (version) {
-      return version.split('.')[0];
-    }
-
-    var Detector = function(options) {
-      angular.extend(this, options);
-      if(angular.isDefined(this.customUA)) {
-        parser.setUA(this.customUA);
-      }
-      this.result = parser.getResult();
-    };
-
-    Detector.prototype.safari = function () {
-      return this.result.browser.name === 'Mobile Safari';
-    };
-
-    Detector.prototype.iOS7 = function () {
-      return this.result.os.name === 'iOS' && getMajorVersion(this.result.os.version) === '7';
-    };
-
-    Detector.prototype.iOS6 = function () {
-      return this.result.os.name === 'iOS' && getMajorVersion(this.result.os.version) === '6';
-    };
-
-    Detector.prototype.device = function () {
-      return this.result.device.model;
-    };
-
-    return Detector;
-
   }]);

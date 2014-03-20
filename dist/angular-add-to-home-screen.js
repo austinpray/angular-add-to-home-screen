0bb46461f4b69d714cc01afa85c07415ea4afd53
Austin Pray <austin@austinpray.com>
2014-03-20 11:22:05 -0500

    build for production

--- dist/angular-add-to-home-screen.js
+++ dist/angular-add-to-home-screen.js
@@ -12,47 +12,6 @@ angular.module('angularAddToHomeScreen', [])
 
 'use strict';
 
-/**
- * 
- */
-angular.module('angularAddToHomeScreen')
-  .factory('$homeScreenDetector', [function(){
-    
-    var parser = new UAParser();
-
-    function getMajorVersion (version) {
-      return version.split('.')[0];
-    }
-
-    var Detector = function(options) {
-      angular.extend(this, options);
-      if(angular.isDefined(this.customUA)) {
-        parser.setUA(this.customUA);
-      }
-      this.result = parser.getResult();
-    };
-
-    Detector.prototype.safari = function () {
-      return this.result.browser.name === 'Mobile Safari';
-    };
-
-    Detector.prototype.iOS7 = function () {
-      return this.result.os.name === 'iOS' && getMajorVersion(this.result.os.version) === '7';
-    };
-
-    Detector.prototype.iOS6 = function () {
-      return this.result.os.name === 'iOS' && getMajorVersion(this.result.os.version) === '6';
-    };
-
-    Detector.prototype.device = function () {
-      return this.result.device.model;
-    };
-
-    return Detector;
-
-  }]);
-'use strict';
-
 angular.module('angularAddToHomeScreen')
   .directive('ngAddToHomeScreen', ['$homeScreenDetector', 'aathsLocales', function($homeScreenDetector, aathsLocales){
     var hydrateInstructions = function (hsdInstance) {
@@ -100,7 +59,6 @@ angular.module('angularAddToHomeScreen')
         $scope.applicable = hsd.safari && (hsd.iOS7() || hsd.iOS6());
         $scope.closeText = "×";
         if($scope.applicable) {
-          console.log(iElm);
           iElm
             .addClass('aaths-container')
             .append(hydrateInstructions(hsd));
@@ -109,4 +67,46 @@ angular.module('angularAddToHomeScreen')
         }
       }
     };
+  }]);
+
+'use strict';
+
+/**
+ * 
+ */
+angular.module('angularAddToHomeScreen')
+  .factory('$homeScreenDetector', [function(){
+    
+    var parser = new UAParser();
+
+    function getMajorVersion (version) {
+      return version.split('.')[0];
+    }
+
+    var Detector = function(options) {
+      angular.extend(this, options);
+      if(angular.isDefined(this.customUA)) {
+        parser.setUA(this.customUA);
+      }
+      this.result = parser.getResult();
+    };
+
+    Detector.prototype.safari = function () {
+      return this.result.browser.name === 'Mobile Safari';
+    };
+
+    Detector.prototype.iOS7 = function () {
+      return this.result.os.name === 'iOS' && getMajorVersion(this.result.os.version) === '7';
+    };
+
+    Detector.prototype.iOS6 = function () {
+      return this.result.os.name === 'iOS' && getMajorVersion(this.result.os.version) === '6';
+    };
+
+    Detector.prototype.device = function () {
+      return this.result.device.model;
+    };
+
+    return Detector;
+
   }]);

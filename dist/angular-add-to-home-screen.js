aac2e9b8f4e509abcbe56bd05498bf9bd6634e10
Chesley Brown <chezbrown@gmail.com>
2014-12-08 21:39:58 -0330

    Updated build with missing iOS8 check

--- dist/angular-add-to-home-screen.js
+++ dist/angular-add-to-home-screen.js
@@ -62,7 +62,7 @@ angular.module('angularAddToHomeScreen')
           }
         };
         var hsd = new $homeScreenDetector();
-        $scope.applicable = hsd.safari && (hsd.iOS7() || hsd.iOS6());
+        $scope.applicable = hsd.safari && (hsd.iOS8() || hsd.iOS7() || hsd.iOS6());
         $scope.closeText = '×';
         if($scope.applicable) {
           iElm

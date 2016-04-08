91af5f53dc5b20b189a7cc5199daf2d550b65b6c
Ties Kuypers <ties@expertees.nl>
2016-04-08 16:55:13 +0200

    Added iOS9

--- js/directives/ngAddToHomeScreen.js
+++ js/directives/ngAddToHomeScreen.js
@@ -7,9 +7,11 @@ angular.module('angularAddToHomeScreen')
       var instructions;
       var icon;
 
-      if(hsdInstance.iOS8() || hsdInstance.iOS7() || hsdInstance.iOS6()) {
+      if(hsdInstance.iOS9() || hsdInstance.iOS8() || hsdInstance.iOS7() || hsdInstance.iOS6()) {
         instructions = aathsLocales.en.iOS;
-        if (hsdInstance.iOS8()) {
+        if (hsdInstance.iOS9()) {
+          icon = 'iOS8';
+        } else if (hsdInstance.iOS8()) {
           icon = 'iOS8';
         } else if (hsdInstance.iOS7()) {
           icon = 'iOS7';
@@ -50,7 +52,7 @@ angular.module('angularAddToHomeScreen')
           }
         };
         var hsd = new $homeScreenDetector();
-        $scope.applicable = hsd.safari() && (hsd.iOS8() || hsd.iOS7() || hsd.iOS6()) && !hsd.fullscreen();
+        $scope.applicable = hsd.safari() && (hsd.iOS9() || hsd.iOS8() || hsd.iOS7() || hsd.iOS6()) && !hsd.fullscreen();
         $scope.closeText = '×';
         if($scope.applicable) {
           iElm
